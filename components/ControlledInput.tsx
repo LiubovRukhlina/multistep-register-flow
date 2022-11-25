e98e9d1148c08e211812bc6f0f46db03f58ce124
LR <liuba.rukhlina@gmail.com>
2022-11-25 13:36:12 +0100

    password show hide

--- components/ControlledInput.tsx
+++ components/ControlledInput.tsx
@@ -3,6 +3,9 @@ import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
 import OutlinedInput from '@mui/material/OutlinedInput';
+import { useState } from 'react';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 type ControlledInputProps = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -18,6 +21,11 @@ function ControlledInput({ control, name, label }: ControlledInputProps) {
     name,
     control,
   });
+  const [isVisible, setVisible] = useState(false);
+
+  const toggle = () => {
+    setVisible(!isVisible);
+  };
 
   return (
     <FormControl variant="standard" error={!!error?.message}>
@@ -29,7 +37,18 @@ function ControlledInput({ control, name, label }: ControlledInputProps) {
         label={label}
         name={field.name} // send down the input name
         inputRef={field.ref} // send input ref, so we can focus on input when error appear
+        type={
+          !isVisible && (name === 'password' || name === 'confirmPassword')
+            ? 'password'
+            : 'text'
+        }
       />
+      {name === 'password' || name === 'confirmPassword' ? (
+        <span className="icon" onClick={toggle}>
+          {isVisible ? <VisibilityOffIcon /> : <VisibilityIcon />}
+        </span>
+      ) : null}
+
       {error ? <FormHelperText>{error.message}</FormHelperText> : null}
     </FormControl>
   );
